911cd04548fda79a2cc0f7306d84f00296e681b6
delmore <jacquiez.romain@orange.fr>
2019-01-14 15:40:38 +0100

    Ready for first test

--- codeRoutes/road.c
+++ codeRoutes/road.c
@@ -1,2 +1,26 @@
 //Inclusion of the prototypes of the functions
 #include "routes.h"
+
+/**
+ * \fn Field initialiseField(int height, int width)
+ * \brief function that initialise our field to make our environment
+ *
+ * \param height : height of the field, must be 1 widen than the original size because the edges is initialized with -1
+ * \param width : width of the field, must be 1 widen than the original size because the edges is initialized with -1
+ * \return Field : A field, which is a tydef declared in core.h (2D array)
+ */
+ road* createRoad(int lengh, int value)
+ {
+     int index;
+
+     road *myRoad;
+
+     myRoad = (road*)malloc(sizeof(int) * lengh);
+
+     for(index = 0; index < lengh; index++)
+     {
+         myRoad[index] = value;
+     }
+
+     return(myRoad);
+ }
--- codeRoutes/road.h
+++ codeRoutes/road.h
@@ -1,5 +1,2 @@
-//Definition of the lengh of a road
-#define LENGH_ROAD (10)
-
-//Définition of the type "road" having a lengh of "LENGH_ROAD"
-typedef int road[LENGH_ROAD];
+//Définition of the type "road" (dynamic allocation)
+typedef int road[];
