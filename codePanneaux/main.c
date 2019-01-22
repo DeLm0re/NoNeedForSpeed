632c6c87c843b6125331aa4f762ebaf0b89a1403
delmore <jacquiez.romain@orange.fr>
2019-01-22 10:29:41 +0100

    add files

---
+++ codePanneaux/main.c
@@ -0,0 +1,23 @@
+
+//General header files
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+//More specific headers files
+#include <time.h>
+#include <math.h>
+#include <string.h>
+#include <stdbool.h>
+
+//Header file
+#include "region.h"
+
+
+int main(int argc, char** argv)
+{
+	// Initializing the random with a seed
+	srand(10495854);
+
+	return 0;
+}
