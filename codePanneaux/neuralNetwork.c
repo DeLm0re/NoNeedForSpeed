d714ae49aa34e5473861b18f5fcf24be49ddc233
Theo <theo.hipault@gmail.com>
2019-01-23 10:46:43 +0100

    Neurone basis

--- codePanneaux/neuralNetwork.c
+++ codePanneaux/neuralNetwork.c
@@ -1,1 +1,48 @@
 #include "neuralNetwork.h"
+
+
+/* @function
+ *      Creates a neurone with default values
+ *      and weights initialized randomly
+ * 
+ * @param
+ *      int weightAbsDimention  :   dimention of the weight matrix in abscissa
+ *      int weightOrdDimention  :   dimention of the weight matrix in ordinate
+ *      Letter targetLetter     :   letter that this neurone can recognize
+ * 
+ * @return  :   neurone created
+ * */
+Neurone *createNeurone(int weightAbsDimention, int weigthOrdDimention, Letter targetLetter)
+{
+    int absIndex, ordIndex;
+
+    //Memory allocation
+	Neurone *neurone = (Neurone*)malloc(sizeof(Neurone));
+
+    neurone->targetLetter = targetLetter;
+
+    //Dimentions of the weight matrix of the neurone
+    neurone->weightAbs = weightAbsDimention;
+    neurone->weightOrd = weigthOrdDimention;
+
+    //Weight matrix allocation
+    neurone->weights = (int**)malloc(weightAbsDimention * sizeof(int*));
+
+    for(absIndex = 0; absIndex < weightAbsDimention; absIndex++)
+    {
+        neurone->weights[absIndex] = (int*)malloc(weigthOrdDimention * sizeof(int));
+    }
+	
+	//Weights equels 0 at the beginning
+	for(absIndex = 0; absIndex < weightAbsDimention; absIndex++)
+    {
+        for(ordIndex = 0; ordIndex < weigthOrdDimention; ordIndex++)
+        {
+            neurone->weights[absIndex][ordIndex] = 0;
+        }
+    }
+	
+	return neurone;
+}
+
+
--- codePanneaux/neuralNetwork.h
+++ codePanneaux/neuralNetwork.h
@@ -1,1 +1,44 @@
 #include "rescale.h"
+
+/* @definition
+ *      Dimentions of the matrix of inputs of a neuron for one letter
+ */
+#define NB_INPUTS_NEURONE 32
+
+/* @type
+ *      Letters of the alphabet and associated values
+ */
+typedef enum{A = 1, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z = 26}Letter;
+
+/* @type
+ *      Structure of a neurone with
+ *      undefined number of input
+ *      
+ * @param
+ * 
+ *      int *weights  :   table of the weights associated with the inputs
+ *      int weightAbs   :   weight dimention in abscissa
+ *      int weightOrd   :   weight dimention in ordinate
+ *      Letter targetLetter :   letter that this neurone can recognize
+ */
+typedef struct Neurone 
+{
+    int **weights;
+    int weightAbs;
+    int weightOrd;
+    Letter targetLetter;
+}Neurone;
+
+
+/* @function
+ *      Creates a neurone with default values
+ *      and weights initialized randomly
+ * 
+ * @param
+ *      int weightAbsDimention  :   dimention of the weight matrix in abscissa
+ *      int weightOrdDimention  :   dimention of the weight matrix in ordinate
+ *      Letter targetLetter     :   letter that this neurone can recognize
+ * 
+ * @return  :   neurone created
+ * */
+Neurone *createNeurone(int weightAbsDimention, int weigthOrdDimention, Letter targetLetter);
